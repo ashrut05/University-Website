d21f4b3630827e43527e5f3b8c3c5e093424f5ad
Ashrut Sharma <ashrut05@protonmail.com>
2020-01-21 21:04:39 +0530

    Release Version 1.0

--- JS/fees.js
+++ JS/fees.js
@@ -1,6 +1,8 @@
 function showDiv(tableId, buttonId) {
+	
     var selectedTable=document.getElementById(tableId);
     var buttonText=document.getElementById(buttonId);
+	
     if (selectedTable.style.display==="table"){
         selectedTable.style.display="none";
         buttonText.value="+";
--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,31 @@
-function scrollToAbout() {
-  window.scrollTo(0, 500);
+/* Ideally notifications will connect to the back end and the database will be used to scroll the notifications but this project is frontend only so I am ignoring all that and just creating a simple function to show the effect of scrolling through notifications */ 
+var counter=1;
+
+function showNotifications(){
+	document.getElementById("notificationInside").style.display="flex";
+	document.getElementById("notificationsButton").style.display="none";
+	
+	document.getElementById("generalNotifications").innerHTML="General Notification " + counter;
+	document.getElementById("examNotifications").innerHTML="Exam Notification " + counter;
+	document.getElementById("eventsNotifications").innerHTML="Events Notification " + counter;
+}
+
+function scrollNotifications(scrollDirection){
+	if(scrollDirection === "next"){
+		if(counter<=2){
+			counter++;
+			document.getElementById("generalNotifications").innerHTML="General Notification " + counter;
+			document.getElementById("examNotifications").innerHTML="Exam Notification " + counter;
+			document.getElementById("eventsNotifications").innerHTML="Events Notification " + counter;
+		}
+	}
+	
+	else {
+		if(counter>=2){
+			counter--;
+			document.getElementById("generalNotifications").innerHTML="General Notification " + counter;
+			document.getElementById("examNotifications").innerHTML="Exam Notification " + counter;
+			document.getElementById("eventsNotifications").innerHTML="Events Notification " + counter;
+		}
+	}
 }
